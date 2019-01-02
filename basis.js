f1aad1f8e23355846debb52b89fdc7a10678e615
Severinelina97 <34237212+Severinelina97@users.noreply.github.com>
2019-01-02 21:23:48 +0100

    Add files via upload
    +js

---
+++ basis.js
@@ -0,0 +1,18 @@
+
+
+//console.log('ikwerk')
+
+//filter knoppen (rustig, druk, minima)
+
+(function() {
+
+const filterButton = document.querySelectorAll('.filter-btn');
+
+console.log(filterButton);
+
+})();
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,34 @@
+
+//de boventste buttons
+//var buttons = document.querySelectorAll(".filter-btn")
+
+//var categorie = document.getElementByClassName(".verhaal");
+
+
+console.log ('ikwerk');
+
+
+
+
+
+
+
+
+
+/*function resultaten (){
+	if (categorie == document.getElementByClassName(".verhaal, .rustigeverhalen")) {
+		categorie.style.display="block";
+	}
+
+	else if (categorie == document.getElementByClassName(".verhaal, .drukkeverhalen")){
+
+categorie.style.display="block";
+
+	}
+
+		else if (categorie == document.getElementByClassName(".verhaal, .minimalistischeverhalen")){
+      categorie.style.display="block";
+		}
+}
+
+item.addEventListener ('click', resultaten);*/
