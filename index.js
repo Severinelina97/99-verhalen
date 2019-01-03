1ff54a99f463d8718bdd4d73b1a02b7032078ab5
Severinelina97 <34237212+Severinelina97@users.noreply.github.com>
2019-01-03 22:57:08 +0100

    Add files via upload
    FILTER FUNCTION werkt nog niet??????

--- index.js
+++ index.js
@@ -9,6 +9,97 @@ console.log ('ikwerk');
 
 
 
+var filterbtn = document.querySelectorAll(".filter-btn");
+
+var articleClass = document.getElementsByTagName("article")[0].className;
+
+var wrapper =document.getElementById("articleWrap");
+
+var wrapperChildren = wrapper.children;
+
+
+//console.log(filterbtn)
+
+
+function myFunction(event){
+
+	    
+	    switch (event) {
+		case  "Rustige verhalen":
+		articleClass = "rustigeverhalen"
+		break;
+
+		case  "Drukke verhalen":
+		articleClass= "drukkeverhalen"
+		break;
+
+		case  "Minimalistische verhalen":
+		articleClass ="minimalistischeverhalen"
+		break;
+	}
+
+	
+
+
+
+
+	for (var i=0 ; i< wrapperChildren.length; i++) {
+		if (wrapperChildren[i].className !== articleClass){
+		wrapperChildren[i].style.display = "none";
+	}
+
+
+
+ 
+
+
+ console.log(event.currentTarget.innerHTML);
+
+
+}
+
+
+
+}
+
+
+function executeClassCheck (){
+
+
+	
+
+	if (articleClass=="verhaal rustigeverhalen"){
+		wrapperChildren.className("drukkeverhalen").style.display="none";
+		wrapperChildren.className("minimalistischeverhalen").style.display="none";
+	}
+
+	if (articleClass="verhaal drukkeverhalen"){
+       wrapperChildren.className("rustigeverhalen").style.display="none";
+        wrapperChildren.className("minimalistischeverhalen").style.display="none";
+	}
+
+	if (articleClass=="verhaal minimalistischeverhalen"){
+		wrapperChildren.className("rustigeverhalen").style.display="none";
+	    wrapperChildren.className("drukkeverhalen").style.display="none";
+	}
+
+
+}
+
+
+for (var i = 0; i < filterbtn.length; i++){
+         filterbtn[i].addEventListener('click', executeClassCheck);
+     console.log(executeClassCheck)
+     }
+
+
+
+
+
+
+
+console.log(articleClass)
+console.log("hallo")
 
 
 
