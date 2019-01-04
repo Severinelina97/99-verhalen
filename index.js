ad07b391361df72a556f9b0f9d2c352d887a1c43
Severinelina97 <34237212+Severinelina97@users.noreply.github.com>
2019-01-04 02:13:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -9,96 +9,68 @@ console.log ('ikwerk');
 
 
 
-var filterbtn = document.querySelectorAll(".filter-btn");
+var filterbtn = document.getElementsByClassName(".filter-btn");
 
-var articleClass = document.getElementsByTagName("article")[0].className;
 
-var wrapper =document.getElementById("articleWrap");
+
+var wrapper =document.getElementById("wrapper");
 
 var wrapperChildren = wrapper.children;
 
 
-//console.log(filterbtn)
-
-
-function myFunction(event){
-
-	    
-	    switch (event) {
-		case  "Rustige verhalen":
-		articleClass = "rustigeverhalen"
-		break;
-
-		case  "Drukke verhalen":
-		articleClass= "drukkeverhalen"
-		break;
-
-		case  "Minimalistische verhalen":
-		articleClass ="minimalistischeverhalen"
-		break;
-	}
-
-	
-
-
-
-
-	for (var i=0 ; i< wrapperChildren.length; i++) {
-		if (wrapperChildren[i].className !== articleClass){
-		wrapperChildren[i].style.display = "none";
-	}
-
-
-
- 
-
-
- console.log(event.currentTarget.innerHTML);
-
-
-}
-
-
-
-}
-
-
-function executeClassCheck (){
-
-
-	
-
-	if (articleClass=="rustigeverhalen"){
-		document.getElementByClassName("drukkeverhalen").style.display="none";
-		document.getElementByClassName("minimalistischeverhalen").style.display="none";
-	}
-
-	if (articleClass="drukkeverhalen"){
-       document.getElementByClassName("rustigeverhalen").style.display="none";
-        document.getElementByClassName("minimalistischeverhalen").style.display="none";
-	}
-
-	if (articleClass=="minimalistischeverhalen"){
-		document.getElementByClassName("rustigeverhalen").style.display="none";
-	    document.getElementByClassName("drukkeverhalen").style.display="none";
-	}
-
-
-}
 
 
 for (var i = 0; i < filterbtn.length; i++){
-         filterbtn[i].addEventListener('click', executeClassCheck);
+         filterbtn[i].addEventListener('click', executeClassCheck);}
+
+     console.log(executeClassCheck);
+
+
+function executeClassCheck (event){
+
+
+	var btnClass = event.currentTarget.classList.item(2);
+
+	var wrapperChildren = document.getElementById("articleWrap").children;
+
+
+
+	for ( i=0; i < wrapperChildren; i++){
+
+		var iets = wrapperChildren[i].classList.item(2)
+
+		if (wrapperChildren[i].classList.item(2) !== btnClass ){
+			wrapperChildren[i].style.display="none";
+		}
+
+		
+
+
+	}
+
      
-     }
 
-console.log(executeClassCheck)
-
-
+     console.log(event.currentTarget.innerHTML);
+}
 
 
 
-console.log(articleClass)
+      
+     
+     
+     
+
+
+
+
+
+
+
+
+
+
+
+
 console.log("hallo")
 
 
@@ -106,20 +78,4 @@ console.log("hallo")
 
 
 
-/*function resultaten (){
-	if (categorie == document.getElementByClassName(".verhaal, .rustigeverhalen")) {
-		categorie.style.display="block";
-	}
 
-	else if (categorie == document.getElementByClassName(".verhaal, .drukkeverhalen")){
-
-categorie.style.display="block";
-
-	}
-
-		else if (categorie == document.getElementByClassName(".verhaal, .minimalistischeverhalen")){
-      categorie.style.display="block";
-		}
-}
-
-item.addEventListener ('click', resultaten);*/
