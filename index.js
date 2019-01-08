536b0eb617ace2c8d67259a7536c22b1cfd70e0b
Severinelina97 <34237212+Severinelina97@users.noreply.github.com>
2019-01-09 00:10:04 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-
-
-
 //check of code werkt ..
-console.log ('ikwerk');
+console.log('ikwerk');
 
 
 
@@ -10,66 +7,73 @@ console.log ('ikwerk');
 //Functie voor het filteren op de categoriepagina - korteverhalen..
 var filterbtn = document.getElementsByClassName("filter-btn");
 
-var wrapper =document.getElementById("wrapper");
+var wrapper = document.getElementById("wrapper");
 
 var wrapperChildren = wrapper.children;
 
 
+//variabelen voor plusbutton
+/*var notificatie = window.getComputedStyle(
+    document.getElementById('.pulse'), ':before')*/
+
+var plusbutton = document.getElementsByClassName("toevoegen");
+
+
 
 // hier worden alle buttons naar geluisterd een voor een gecontroleerd op classname
-for (var i = 0; i < filterbtn.length; i++){
+for (var i = 0; i < filterbtn.length; i++) {
     filterbtn[i].addEventListener('click', executeClassCheck);
 }
 
 
 // dit is de functie vervolgens word uitgevoerd vanuit de eventlistener
-function executeClassCheck (event){
+function executeClassCheck(event) {
 
-	var btnClass = event.currentTarget.classList.item(1);
+    var btnClass = event.currentTarget.classList.item(1);
 
-	var wrapperChildren = document.getElementById("wrapper").children;
+    var wrapperChildren = document.getElementById("wrapper").children;
 
 
-	for ( i=0; i < wrapperChildren.length; i++){
+    for (i = 0; i < wrapperChildren.length; i++) {
 
-		var iets = wrapperChildren[i].classList.item(1)
+        var iets = wrapperChildren[i].classList.item(1)
 
-		if (wrapperChildren[i].classList.item(1) !== btnClass ){
-			wrapperChildren[i].style.display="none";
-		} else {
-			wrapperChildren[i].style.display="";
-		}
+        if (wrapperChildren[i].classList.item(1) !== btnClass) {
+            wrapperChildren[i].style.display = "none";
+        } else {
+            wrapperChildren[i].style.display = "";
+        }
 
-		// bij deze functie word gekeken of de class matcht van de button met de verhalen
-		// als dit klopt dan worden alleen de verhalen laten zien die matchen met de button
-     delay (600)
+        // bij deze functie word gekeken of de class matcht van de button met de verhalen
+        // als dit klopt dan worden alleen de verhalen laten zien die matchen met de button
+        
 
-	}
-
-     
-
-     console.log(event.currentTarget.innerHTML);
+    }
+    
 }
 
 
-
-      
-     
-     
-     
+//functie voor toevoegknop
 
 
 
+    plusbutton.addEventListener("click", checked(){
 
+        plusbutton.classList.add("checked")
 
-
-
+    }
+    
 
 
 
 
 
-console.log("hallo")
+
+
+    
+
+
+
 
 
 
